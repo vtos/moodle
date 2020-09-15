f52b7ad5aef3748822e3000ed0860e715e1682c5
Vitaly <potenkov@gmail.com>
2020-09-15 20:30:59 +0300

    MDL-35869 admin: sort users lists by lastname in bulk actions
    Users lists in bulk user actions were sorted by fullname which actually resulted
    in being sorted by their firstname.
    The fix injects usage of 'users_order_by_sql()' defined in '/lib/datalib.php'.
    To define the second parameter of 'users_order_by_sql()' which is a search string
    to identify the exact matches when ordering the users, the new function
    'get_search_value_from_user_filtering()' was defined in the same file
    '/admin/user/lib.php'. The function fetches the user filtering from the $SESSION
    global variable and returns the string to pass to 'users_order_by_sql()'.
    For the details on that see PhpDocs for 'users_order_by_sql()'. Also, some comments
    are contained within the definition of 'get_search_value_from_user_filtering()'.

--- admin/user/lib.php
+++ admin/user/lib.php
@@ -23,6 +23,11 @@ function add_selection_all($ufiltering) {
 function get_selection_data($ufiltering) {
     global $SESSION, $DB, $CFG;
 
+    // Get the 'search' parameter for 'users_order_by_sql()' from the session's user filtering.
+    $search = get_search_value_from_user_filtering();
+
+    list($orderbysql, $orderbyparams) = users_order_by_sql('', $search);
+
     // get the SQL filter
     list($sqlwhere, $params) = $ufiltering->get_sql_filter("id<>:exguest AND deleted <> 1", array('exguest'=>$CFG->siteguest));
 
@@ -31,7 +36,11 @@ function get_selection_data($ufiltering) {
     $scount = count($SESSION->bulk_users);
 
     $userlist = array('acount'=>$acount, 'scount'=>$scount, 'ausers'=>false, 'susers'=>false, 'total'=>$total);
-    $userlist['ausers'] = $DB->get_records_select_menu('user', $sqlwhere, $params, 'fullname', 'id,'.$DB->sql_fullname().' AS fullname', 0, MAX_BULK_USERS);
+    if ($orderbyparams) {
+        $params += $orderbyparams;
+    }
+    $userlist['ausers'] = $DB->get_records_select_menu('user', $sqlwhere, $params, $orderbysql,
+        'id,'.$DB->sql_fullname().' AS fullname', 0, MAX_BULK_USERS);
 
     if ($scount) {
         if ($scount < MAX_BULK_USERS) {
@@ -39,9 +48,61 @@ function get_selection_data($ufiltering) {
         } else {
             $bulkusers = array_slice($SESSION->bulk_users, 0, MAX_BULK_USERS, true);
         }
-        list($in, $inparams) = $DB->get_in_or_equal($bulkusers);
-        $userlist['susers'] = $DB->get_records_select_menu('user', "id $in", $inparams, 'fullname', 'id,'.$DB->sql_fullname().' AS fullname');
+        list($in, $inparams) = $DB->get_in_or_equal($bulkusers, SQL_PARAMS_NAMED);
+        if ($orderbyparams) {
+            $inparams += $orderbyparams;
+        }
+        $userlist['susers'] = $DB->get_records_select_menu('user', "id $in", $inparams, $orderbysql,
+            'id,'.$DB->sql_fullname().' AS fullname');
     }
 
     return $userlist;
+}
+
+/**
+ * Fetches the user filtering from $SESSION if applied
+ * and uses it's fields to define the search string for users_order_by_sql
+ *
+ * @package core
+ * @return string search string to use as a parameter in users_order_by_sql
+ */
+function get_search_value_from_user_filtering() {
+    global $SESSION, $PAGE;
+
+    $search = null;
+
+    if ($SESSION->user_filtering) {
+        // Available fields for the 'search' parameter.
+        $fieldstocheck = array_merge(array('realname', 'firstname', 'lastname'), get_extra_user_fields($PAGE->context));
+
+        // The assumption is that the 'search' parameter for 'users_order_by_sql()' does make sense
+        // only for text fields like 'lastname', 'institution', 'city/town', etc.
+        // and which are set in the 'showuseridentity' admin setting.
+        if ($countrykey = array_search('country', $fieldstocheck)) {
+            unset($fieldstocheck[$countrykey]);
+        }
+
+        // We have to use the 'magic numbers' here because the comparison operators for the text fields which are defined
+        // in /user/filters/text.php in the 'getOperators()' method do not have any constants defined.
+        // We're only interested in 0, 2, 3, 4 which correspond to the 'contains',
+        // 'isequalto', 'startswith' and 'endswith' text operators.
+        $searchoperators = array(0, 2, 3, 4);
+
+        // Fetch the value of the first appropriate filtering field to set the 'search'.
+        foreach ($fieldstocheck as $fieldtocheck) {
+            if ($search) {
+                break;
+            }
+            if (array_key_exists($fieldtocheck, $SESSION->user_filtering)) {
+                foreach ($SESSION->user_filtering[$fieldtocheck] as $filteringfield) {
+                    if (! in_array($filteringfield['operator'], $searchoperators)) {
+                        continue;
+                    }
+                    $search = $filteringfield['value'];
+                }
+            }
+        }
+    }
+
+    return $search;
 }
